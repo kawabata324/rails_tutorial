884a5ef55bc40c450974b4f9e98a50aae2d62d36
kawabata <s1931115@s.konan-u.ac.jp>
2021-12-30 13:50:58 +0900

    4章まで完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
 
-  def hello
-    render html: "Hello World"
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name, :gender])
   end
 end
